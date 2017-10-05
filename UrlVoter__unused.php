0559a957326068d582b0218c244b9b52e52b590e
Nardberjean <Nardberjean@users.noreply.github.com>
2017-10-04 21:22:36 -0400

    Update UrlVoter__unused.php

--- UrlVoter__unused.php
+++ UrlVoter__unused.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Doctrine DBAL Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Security;
 
 use AppBundle\Entity\URL;
@@ -7,7 +16,7 @@ use AppBundle\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
-class UrlVoter extends Voter
+class UrlVoter__unused extends Voter
 {
     // these strings are just invented: you can use anything
     const VIEW = 'view';
